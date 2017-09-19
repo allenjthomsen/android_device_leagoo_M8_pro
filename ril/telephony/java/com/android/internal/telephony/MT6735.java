c0e0c829a7cfd37a25cb2e53c9656da39b9ee4e8
hyperion70 <bmaximov71@gmail.com>
2017-09-19 14:05:17 +0300

    Update and clean

--- ril/telephony/java/com/android/internal/telephony/MT6735.java
+++ ril/telephony/java/com/android/internal/telephony/MT6735.java
@@ -55,6 +55,7 @@ public class MT6735 extends RIL implements CommandsInterface {
     private static final int RIL_REQUEST_EMERGENCY_DIAL = 2087;
     private static final int RIL_REQUEST_SET_ECC_SERVICE_CATEGORY = 2088;
     private static final int RIL_REQUEST_SET_ECC_LIST = 2089;
+    private static final int REFRESH_SESSION_RESET = 6;      /* Session reset */
 
     private int[] dataCallCids = { -1, -1, -1, -1, -1 };
 
@@ -62,7 +63,7 @@ public class MT6735 extends RIL implements CommandsInterface {
     private TelephonyManager mTelephonyManager;
     private MtkEccList mEccList;
 
-    public MT6735(Context context, int preferredNetworkType, int cdmaSubscription) {
+   public MT6735(Context context, int preferredNetworkType, int cdmaSubscription) {
         super(context, preferredNetworkType, cdmaSubscription, null);
         //mContext = context;
         Rlog.i("MT6735", "Ctor1: context is " + mContext);
@@ -260,6 +261,13 @@ public class MT6735 extends RIL implements CommandsInterface {
         String rawefId = p.readString();
         response.efId   = rawefId == null ? 0 : Integer.parseInt(rawefId);
         response.aid = p.readString();
+        if (response.refreshResult > IccRefreshResponse.REFRESH_RESULT_RESET) {
+            if (response.refreshResult == REFRESH_SESSION_RESET) {
+                response.refreshResult = IccRefreshResponse.REFRESH_RESULT_RESET;
+            } else {
+                response.refreshResult = IccRefreshResponse.REFRESH_RESULT_INIT;
+            }
+        }
 
         return response;
     }
